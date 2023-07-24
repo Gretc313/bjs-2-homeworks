9aaba8600c0600eb6c0d0d2ca7d924cb166b55c2
Raimon <langertage@gmail.com>
2023-07-24 16:25:01 +0300

    task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d;
+	d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		arr = [];
+	} else if (d === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+	} else {
+		let x1 = (-b + Math.sqrt(d)) / (2 * a);
+		let x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2)
+	}
+	return arr;
 }
 
+solveEquation(a, b, c);
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let p = (percent / 100) / 12;
+	let s = amount - contribution;
+	let monthlyPayment = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+	let totalMortgage = +(monthlyPayment * countMonths).toFixed(2);
+
+	return (totalMortgage);
 }
+calculateTotalMortgage(percent, contribution, amount, countMonths);
