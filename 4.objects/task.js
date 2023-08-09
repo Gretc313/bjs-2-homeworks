0da63aabab98be91c921e0d50d50ca17cd4c27ca
Raimon <langertage@gmail.com>
2023-08-09 19:25:04 +0300

    Task Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,52 @@
 function Student(name, gender, age) {
-  
+
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+
+}
+let student = {
+	marks: [],
+}
+Student.prototype = student;
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (marks in Student) {
+		for (let i = 0; i < marksToAdd.length; i++) {
+			this.marks.push(marksToAdd[i]);
+		}
+	} else {
+		return 0;
+	}
+
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks.lenght === 0) {
+		return 0;
+	} else {
+		let sum = this.marks[0];
+		let average;
+		for (let i = 0; i < this.marks.length; i++) {
+			sum += this.marks[i];
+		}
+		average = sum / this.marks.length;
+		return average;
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.exclude = function(reason) {
+	delete Student.subject;
+	delete Student.marks;
+	Student.excluded = reason;
 }
-
-Student.prototype.exclude = function (reason) {
-  
-}
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
